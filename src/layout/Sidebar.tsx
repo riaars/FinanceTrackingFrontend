03a24e248d6f7f56a0616104c3e239ab9b746489
riaars <riaratnasari@outlook.com>
2025-02-24 02:13:39 +0100

    update sidebar active state

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,38 +1,37 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import * as PATH from "../config/Path";
 
 function Sidebar() {
   const navigate = useNavigate();
 
+  const location = useLocation();
+
+  const sideMenus = [
+    {
+      title: "Dashboard",
+      path: PATH.DASHBOARD,
+    },
+    { title: "Add Transaction", path: PATH.ADD_NEW_TRANSACTION },
+    { title: "Transaction", path: PATH.TRANSACTIONS },
+    { title: "Settings", path: PATH.SETTINGS },
+  ];
+
+  const activeMenu = (menu_path: string) => {
+    return location.pathname === menu_path ? "active" : "inherit";
+  };
+
   return (
     <div className="sidebar-container">
       <ul>
-        <li
-          className="sidebar-container__item"
-          onClick={() => navigate(PATH.DASHBOARD)}
-        >
-          Dashboard
-        </li>
-        <li
-          className="sidebar-container__item"
-          onClick={() => navigate(PATH.ADD_NEW_TRANSACTION)}
-        >
-          Add Transaction
-        </li>
-        <li
-          className="sidebar-container__item"
-          onClick={() => navigate(PATH.TRANSACTIONS)}
-        >
-          Transactions
-        </li>
-
-        <li
-          className="sidebar-container__item"
-          onClick={() => navigate(PATH.SETTINGS)}
-        >
-          Settings
-        </li>
+        {sideMenus.map((sideMenu) => (
+          <li
+            className={`sidebar-container__item ${activeMenu(sideMenu.path)}`}
+            onClick={() => navigate(sideMenu.path)}
+          >
+            {sideMenu.title}
+          </li>
+        ))}
       </ul>
     </div>
   );
